c1f47a97aa94e07722ed524f9e8fa9d932d153a2
Macbook Air <macbookair@MacBook-Airs-Phuc-Thinh.local>
2022-03-10 08:08:43 +0700

    update 10.3

---
+++ 6.tong-quang-js/1. Study.js
@@ -0,0 +1,46 @@
+        // alert("Created website");
+        // var b = prompt("Insert data");
+        // console.log("Data's B:  ", b);
+
+    // var b = prompt('Insert data');
+    // console.log("Data's A: ",b);
+    // console.log("Data's B: ", 1);
+    // console.log("Total= : ", b + 1);
+
+    // var x,y,z;
+    // var x = prompt('Data x');
+    // var y = prompt('Data y');
+    // // console.log("x = ",x);
+    // // console.log("y = ",y);
+
+    // z = parseInt(x) + parseInt(y);
+    // alert('Gia tri z la: '+z); 
+    // // console.log("z = ",+z);
+
+    // alert('Tinh');
+    // prompt('data')
+    // confirm("Please reset your computer")
+    
+    
+    // function showMesaage(){
+    //     document.write('xinchao');
+    // }
+
+    // var i = 10;
+    // i++;
+    // var chuoi = "Data's i la";
+    // var chuoi1 = chuoi + i;
+    // alert("Result's i = ", + i);
+
+    var check1 = false;
+    var check2 = true;
+    var check = check1&&check2;
+    alert ("Result's check : ",check);
+
+    
+
+
+
+
+        
+
