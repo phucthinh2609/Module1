632f202b290025cecad2fc0b5ffe3444e533e65b
phucthinh2609 <phucthinh2609@gmail.com>
2022-03-16 14:53:22 +0700

    add 9_TH_Ball

---
+++ 0_Vo-long/JS/AdditionalAndLoop/index.js
@@ -0,0 +1,13 @@
+let dtb = 8.7;
+
+    if (dtb >= 9 && dtb <= 10) {
+        console.log('Xuat sac')
+    }else if (dtb >= 8) {
+        console.log('Gioi')
+    }else if (dtb >= 7) {
+        console.log('Kha')
+    }else if (dtb >= 5) {
+        console.log ('Trung binh')
+    }else {
+        console.log ('Yeu')
+    }
